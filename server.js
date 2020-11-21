c68892ad2287564bdd12822723baa7b904727dcd
frank salvo <fhsalvo@yahoo.com>
2020-11-21 00:49:09 -0500

    removed json data from handlebars files into js files in public folder;  on Review page - activated radio buttons to match returned data and separated activity and topics fields to match explore page;  cleaned up comments and stry junk

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ const path = require('path');
 const exphbs = require('express-handlebars')
 
 app.use(express.static("public"));
-app.set('port', (process.env.PORT || 8000));
+const PORT = process.env.PORT || 8000;
+// app.set('port', (process.env.PORT || 8000));
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -30,6 +31,15 @@ app.use(express.json());
 var routes = require("./controllers/bucketListController.js");
 app.use(routes);
 
-app.listen(app.get('port'), function(){
-    console.log('Server started on port '+app.get('port'))
-});
+app.get('/', function(req, res){
+    res.redirect('/explore');
+ });
+
+// app.listen(app.get('port'), function(){
+//     console.log('Server started on port '+app.get('port'))
+// });
+
+// listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});  
